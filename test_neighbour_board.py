03e634922eecb45b1285fe5976b22204dd4e12ae
Bruce W. Lowther <brucelowther@gmail.com>
2021-03-21 11:33:06 -0400

    More detail testing.

--- test_neighbour_board.py
+++ test_neighbour_board.py
@@ -16,11 +16,14 @@ class TestBoard(TestCase):
 
     def test_two_by_two_board_neighbours(self):
         b = board.Board((2, 2))
-        target_coord = (0,0)
+        target_coord = (0, 0)
         neighbour_coord = list(b.neighbours(target_coord, include_diagonals=True))
         self.assertEqual(3, len(neighbour_coord))
+        self.assertSetEqual({(0, 1), (1, 0), (1, 1)}, set(neighbour_coord))
+
         neighbour_coord = list(b.neighbours(target_coord, include_diagonals=False))
         self.assertEqual(2, len(neighbour_coord))
+        self.assertSetEqual({(0, 1), (1, 0)}, set(neighbour_coord))
 
     def test_three_by_three_board_neighbours(self):
         b = board.Board((3,3))
